a8f8d266a0dc1df382da8b22ee90b7c43010af77
Rehan Raju <20068389@mydbs.ie.>
2025-11-23 04:40:41 +0530

    navbar updated

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const authRoute = require("./routes/auth");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use("/api/auth", authRoute);
 
 module.exports = app;
