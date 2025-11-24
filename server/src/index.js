299ab7a2e60e841cbc6cd0c7c1f3dd3679f69943
Rehan Raju <20068389@mydbs.ie.>
2025-11-24 18:58:06 +0530

    updated models

--- server/src/index.js
+++ server/src/index.js
@@ -2,9 +2,18 @@ require("dotenv").config();
 const app = require("./app");
 const sequelize = require("./config/database");
 
-sequelize.sync().then(() => {
-  console.log("DB synced");
-  app.listen(process.env.PORT, () =>
-    console.log(`Server running on ${process.env.PORT}`)
-  );
+
+const PORT = process.env.PORT || 4000;
+
+  app.listen(PORT,async () => {
+    console.log(`Server running on ${PORT}`);
+  
+
+  try {
+    await sequelize.authenticate();
+    console.log("DB Connection Is OK !!");
+  } catch (error) {
+    console.log("DB Connection Failed",err);
+  }
+
 });
---
+++ server/src/models/index.js
@@ -0,0 +1,21 @@
+// referance code from chatgpt
+const Sequelize = require('sequelize');
+const sequelize = require('../config/database');
+
+const Car = require('./car')(sequelize, Sequelize.DataTypes);
+const Cook = require('./cook')(sequelize, Sequelize.DataTypes);
+const Booking = require('./booking')(sequelize, Sequelize.DataTypes);
+const User = require('./user')(sequelize, Sequelize.DataTypes);
+
+Car.hasMany(Booking, { foreignKey: 'service_id', constraints: false, scope: { service_type: 'car' }});
+Cook.hasMany(Booking, { foreignKey: 'service_id', constraints: false, scope: { service_type: 'cook' }});
+Booking.belongsTo(Car, { foreignKey: 'service_id', constraints: false });
+Booking.belongsTo(Cook, { foreignKey: 'service_id', constraints: false });
+
+module.exports = {
+  sequelize,
+  Car,
+  Cook,
+  Booking,
+  User
+};
