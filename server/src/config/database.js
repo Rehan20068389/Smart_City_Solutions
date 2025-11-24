c0903d847e20f9d420315f6ef917e81d19e7fd5b
Rehan Raju <20068389@mydbs.ie.>
2025-11-24 20:07:26 +0530

    user table created

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -11,3 +11,4 @@ module.exports = new Sequelize(
     logging: false
   }
 );
+
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -2,11 +2,13 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../config/database');
 
+//importing models
 const Car = require('./car')(sequelize, Sequelize.DataTypes);
 const Cook = require('./cook')(sequelize, Sequelize.DataTypes);
 const Booking = require('./booking')(sequelize, Sequelize.DataTypes);
 const User = require('./user')(sequelize, Sequelize.DataTypes);
 
+//associations
 Car.hasMany(Booking, { foreignKey: 'service_id', constraints: false, scope: { service_type: 'car' }});
 Cook.hasMany(Booking, { foreignKey: 'service_id', constraints: false, scope: { service_type: 'cook' }});
 Booking.belongsTo(Car, { foreignKey: 'service_id', constraints: false });
