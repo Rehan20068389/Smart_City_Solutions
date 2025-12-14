3a10466c1099100f0799a57d53358607820f51ca
Rehan Raju <20068389@mydbs.ie.>
2025-12-14 01:15:22 +0000

    Fix DB connection error variable

--- server/index.js
+++ server/index.js
@@ -2,18 +2,15 @@ require("dotenv").config();
 const app = require("./app");
 const sequelize = require("./src/config/database");
 
-
 const PORT = process.env.PORT || 4000;
 
-  app.listen(PORT,async () => {
-    console.log(`Server running on ${PORT}`);
-  
+app.listen(PORT, async () => {
+  console.log(`Server running on ${PORT}`);
 
   try {
     await sequelize.authenticate();
     console.log("DB Connection Is OK !!");
   } catch (error) {
-    console.error("DB Connection Failed",err);
+    console.error("DB Connection Failed:", error.message);
   }
-
 });
