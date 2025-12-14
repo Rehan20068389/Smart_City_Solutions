1783301c341801f7f153045e85b6271a4ebc238e
Rehan Raju <20068389@mydbs.ie.>
2025-12-14 00:08:35 +0000

    change the files

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const app = require("./app");
+const sequelize = require("./server/src/config/database");
+
+
+const PORT = process.env.PORT || 4000;
+
+  app.listen(PORT,async () => {
+    console.log(`Server running on ${PORT}`);
+  
+
+  try {
+    await sequelize.authenticate();
+    console.log("DB Connection Is OK !!");
+  } catch (error) {
+    console.log("DB Connection Failed",err);
+  }
+
+});
