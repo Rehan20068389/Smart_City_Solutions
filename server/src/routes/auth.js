b3d702542152fc43bd8344593665fbca44425ccd
Rehan Raju <20068389@mydbs.ie.>
2025-12-14 01:01:52 +0000

    Resolve merge conflicts

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
 const authMiddleware = require('../middlewares/auth');
-<<<<<<< HEAD
 const ctrl = require('../controllers/authcontroller.js');
-=======
-const ctrl = require('../controllers/authcontroller');
->>>>>>> 9072ced1ebc8694fcd938757c68b9af22d96f0dc
 
 router.post('/signup', ctrl.signup);
 router.post('/login', ctrl.login);
