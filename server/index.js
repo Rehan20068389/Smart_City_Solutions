cc679c607d12ee07ef2192852a0921a103c22f54
Rehan Raju <20068389@mydbs.ie.>
2025-12-14 23:08:13 +0000

    deployment Fix databse path in index.js

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@
 // });
 //
 const express = require('express');
-const sequelize = require('./config/database');
+const sequelize = require('./config/database.js');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
